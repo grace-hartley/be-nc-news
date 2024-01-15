878d895a4d38dd4226c89b9f9af03afe7d7e3878
grace-hartley <gracerita.hartley@gmail.com>
2024-01-15 15:27:08 +0000

    completed Q3 GET /api

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controller");
+const { getApi } = require("./controllers/api.controller");
+
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", getApi);
 
 // Error handling
 
