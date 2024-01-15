08c22ba2ac30b8d06c3f6a7b670bc8952e7a3eed
grace-hartley <gracerita.hartley@gmail.com>
2024-01-15 17:04:56 +0000

    completed Q4 GET /api/articles/:article_id

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controller");
 const { getApi } = require("./controllers/api.controller");
+const { getArticleById } = require("./controllers/articles.controller");
 
 const app = express();
 
@@ -10,10 +11,28 @@ app.get("/api/topics", getTopics);
 
 app.get("/api", getApi);
 
+app.get("/api/articles/:article_id", getArticleById);
+
 // Error handling
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "path does not exist" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.msg === "Not Found") {
+    res.status(404).send({ msg: "article does not exist" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
+  }
 });
 
 module.exports = app;
